c30a916d378b13b27da4c05ff2dfd25e353d0577
Sunmeet Singh <renu.kabeer@gmail.com>
2022-06-16 20:31:47 +0530

    Itinerary added

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ console.log(manali)
 manali.forEach((ele) => {
     console.log(ele)
     ele.addEventListener('click', function () {
-        window.open('./Iterinary/Manali-kasol-compressed.pdf');
+        window.open('./Iterinary/Manali-kasol-compressed (3).pdf');
     })
 })
