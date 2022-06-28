23b1a078ff97816afad0eb9aa91a3fafcfe5a556
Sunmeet Singh <renu.kabeer@gmail.com>
2022-06-28 17:57:59 +0530

    Adding email functionality and database created

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+let inputForms = document.querySelectorAll('.NyXXA')
+let database = {}
+let submitBtn = document.querySelector("input[type='submit']")
+for (let i = 0; i<inputForms.length; i++){
+    
+    let inputForm = inputForms[i];
+    let word = ""
+    inputForm.addEventListener('keyup',(e)=>{
+        word=e.target.value
+        console.log(word)
+        let name = inputForm.name
+        database[name] = word
+        console.log(database)
+    })
+    let arr = [];
+    arr.push(database)
+    submitBtn.addEventListener('click',()=>{
+        window.open(`mailto:renusonykabeer@gmail.com?subject=subject&body=${database}`)
+    })
+}
+
 let manali = document.querySelectorAll('[class="swiper-wrapper"]>div')
 let manaliObj = {Destination: "Manali"}
 let input = document.querySelector('.jYtkWQ')
@@ -12,4 +33,5 @@ manali.forEach((ele) => {
     })
 
 })
+
 
